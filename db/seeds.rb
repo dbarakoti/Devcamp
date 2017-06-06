917213b35170bc6820ff72d88d15fa42a57d263d
Dipesh Barakoti <dbarakoti@gmail.com>
2017-06-06 14:46:52 -0600

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,8 @@ end
 		title: "Portfolio title: #{portfolio_item}",
 		subtitle: "My great service",
 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			main_image:"http://placehold.it/600*400",
-			thumb_image:"http://placehold.it/350*200" 
+			main_image:"http://via.placeholder.com/600x400",
+			thumb_image:"http://via.placeholder.com/350x200"
 
 	)
 end
