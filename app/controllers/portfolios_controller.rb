05d8d7e5e7efd5321e26dd2dd0b3a46a21c66651
Dipesh Barakoti <dbarakoti@gmail.com>
2017-06-12 14:49:03 -0600

    Implemented Image defaults for portfolio-items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,6 +11,7 @@ class PortfoliosController < ApplicationController
 	def new
 		@portfolio_item = Portfolio.new
 	end
+  
 	def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitile, :body))
 
